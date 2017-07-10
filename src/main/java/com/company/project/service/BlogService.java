968a122a2cff757a4ecd7457025fe756cce51ea2
laileon <leon@igola.com>
2017-07-10 20:02:26 +0800

    init

---
+++ src/main/java/com/company/project/service/BlogService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.Blog;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2017/07/10.
+ */
+public interface BlogService extends Service<Blog> {
+
+}
