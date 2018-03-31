dbd6a71aef9ad2d1e4b640777096d6e1517bb0a6
laileon <leon@igola.com>
2018-03-31 23:08:04 +0800

    schedule shell

--- src/main/java/com/company/project/web/ArticleController.java
+++ src/main/java/com/company/project/web/ArticleController.java
@@ -15,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Leon Lai on 2018/03/30.
@@ -33,29 +34,29 @@ public class ArticleController {
 
     RestTemplate restTemplate = new RestTemplate();
 
-    //每10分钟启动
-    @Scheduled(cron = "0 0/10 * * * ?")
+    //test 1 min
+    @Scheduled(cron = "0 0/10 * * * 1-5")
     public void timerToNow() {
 
-        System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+//        System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         String url = "http://www3.nhk.or.jp/news/easy/news-list.json";
         String result = restTemplate.getForObject(url, String.class, new HashMap<>());
         getArticleData(result);
+//        runShell("k10011351191000");
+//        runShell("k10011382561000");
 //        System.out.println(result);
     }
 
     private void getArticleData(String article) {
         List<Article> articleListDB = articleService.findAll();
+        List<Article> articleList = new ArrayList<>();
         try {
             String newStr1 = article.substring(3, article.length() - 3);
             String formatString = newStr1.replace("\":[", NHK);
             //date based
             String[] stringArray = formatString.split("],");
-            Map<String, List<Article>> map = new HashMap<>();
             for (int j = 0; j < stringArray.length - 1; j++) {
                 String[] dateArray = stringArray[j].split(NHK);
-                //date
-                String date = dateArray[0].replace("\"", "");
                 String formatString2 = dateArray[1].replace("},{", NHK).replace("}", "").replace("{", "");
                 String[] jsonArr = formatString2.split(NHK);
                 System.out.println(jsonArr[0]);
@@ -63,12 +64,42 @@ public class ArticleController {
                     String jsonStr = "{" + aJsonArr + "}";
                     Article articleModel = new Gson().fromJson(jsonStr, Article.class);
 //                    System.out.println(articleModel.getNews_creation_time());
-                    if (!articleListDB.contains(articleModel)){
+                    if (!articleListDB.contains(articleModel)) {
+                        articleList.add(articleModel);
                         articleService.save(articleModel);
                     }
                 }
             }
+            if (articleList.size() > 0) {
+                for(Article article1 : articleList){
+                    runShell(article1.getNews_id());
+                }
+            }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void runShell(String newsId) {
+        String returnCode = "";
+        try {
+//            /Users/laileon/Documents/test.sh
+            Process process = Runtime.getRuntime().exec("chmod 755 /home/ffmpeg.sh");
+            process.waitFor();
+            // test2.sh是要执行的shell文件，param1参数值，test2.sh和param1之间要有空格
+            // 多个参数可以在param1后面继续增加，但不要忘记空格！！
+            process = Runtime.getRuntime().exec("/home/ffmpeg.sh" + " " + newsId);
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            StringBuilder sb = new StringBuilder("");
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+            System.out.println(sb.toString());
+            returnCode = process.waitFor() + "";
+            System.out.println(returnCode);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
