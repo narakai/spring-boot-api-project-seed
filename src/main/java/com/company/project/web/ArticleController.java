d2c228aaf73bdbddb10f14026d12531b18debf0f
laileon <leon@igola.com>
2018-04-10 15:33:47 +0800

    schedule issue

--- src/main/java/com/company/project/web/ArticleController.java
+++ src/main/java/com/company/project/web/ArticleController.java
@@ -16,7 +16,9 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,7 +38,7 @@ public class ArticleController {
     @Scheduled(cron = "0 0/5 * * * 1-5")
     public void timerToNow() {
 
-//        System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         String url = "http://www3.nhk.or.jp/news/easy/news-list.json";
         String result = restTemplate.getForObject(url, String.class, new HashMap<>());
         getArticleData(result);
@@ -47,6 +49,7 @@ public class ArticleController {
 
     private void getArticleData(String article) {
         List<Article> articleListDB = articleService.findAll();
+        System.out.println("DB " + articleListDB.size());
         List<Article> articleList = new ArrayList<>();
         try {
             String newStr1 = article.substring(3, article.length() - 3);
@@ -57,9 +60,11 @@ public class ArticleController {
                 String[] dateArray = stringArray[j].split(NHK);
                 String formatString2 = dateArray[1].replace("},{", NHK).replace("}", "").replace("{", "");
                 String[] jsonArr = formatString2.split(NHK);
+                System.out.println("NET " + jsonArr.length);
                 for (String aJsonArr : jsonArr) {
                     String jsonStr = "{" + aJsonArr + "}";
                     Article articleModel = new Gson().fromJson(jsonStr, Article.class);
+                    System.out.println("NewsID: " + articleModel.getNews_id());
                     if (!articleListDB.contains(articleModel)) {
                         articleList.add(articleModel);
                         articleService.save(articleModel);
@@ -68,17 +73,18 @@ public class ArticleController {
             }
             if (articleList.size() > 0) {
                 for (Article article1 : articleList) {
-//                    System.out.println(" article1 " + article1.getNews_id());
+                    System.out.println(" article1 " + article1.getNews_id());
                     runShell(article1.getNews_id());
                 }
             }
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private void runShell(String newsId) {
-        String returnCode = "";
+    private synchronized void runShell(String newsId) {
+        int returnCode = -1;
         try {
 //            /Users/laileon/Documents/test.sh
 //            Process process = Runtime.getRuntime().exec("chmod 755 /home/ffmpeg.sh");
@@ -86,17 +92,24 @@ public class ArticleController {
             // test2.sh是要执行的shell文件，param1参数值，test2.sh和param1之间要有空格
             // 多个参数可以在param1后面继续增加，但不要忘记空格！！
             System.out.println("IM IN");
-            Process process = Runtime.getRuntime().exec("/home/ffmpeg.sh" + " " + newsId);
-            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = null;
-            StringBuilder sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            br.close();
-            printMessage(process.getInputStream());
-            printMessage(process.getErrorStream());
-            returnCode = process.waitFor() + "";
+//            String cmd = "/home/ffmpeg.sh" + " " + newsId;
+//            ProcessBuilder builder = new ProcessBuilder("bash", "-c",cmd);
+            String[] command = {"/home/ffmpeg.sh", newsId};
+            ProcessBuilder builder = new ProcessBuilder(command);
+            builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+            builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+            Process process = builder.start();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//            String line = null;
+//            StringBuilder sb = new StringBuilder();
+//            while ((line = br.readLine()) != null) {
+//                sb.append(line);
+//            }
+//            br.close();
+//            printMessage(process.getInputStream());
+//            printMessage(process.getErrorStream());
+            returnCode = process.waitFor();
             System.out.println("code " + returnCode);
         } catch (IOException | InterruptedException e) {
             System.out.println(e.getMessage());
@@ -122,23 +135,23 @@ public class ArticleController {
         }).start();
     }
 
-    @PostMapping("/add")
-    public Result add(Article article) {
-        articleService.save(article);
-        return ResultGenerator.genSuccessResult();
-    }
-
-    @PostMapping("/delete")
-    public Result delete(@RequestParam Integer id) {
-        articleService.deleteById(id);
-        return ResultGenerator.genSuccessResult();
-    }
-
-    @PostMapping("/update")
-    public Result update(Article article) {
-        articleService.update(article);
-        return ResultGenerator.genSuccessResult();
-    }
+//    @PostMapping("/add")
+//    public Result add(Article article) {
+//        articleService.save(article);
+//        return ResultGenerator.genSuccessResult();
+//    }
+//
+//    @PostMapping("/delete")
+//    public Result delete(@RequestParam Integer id) {
+//        articleService.deleteById(id);
+//        return ResultGenerator.genSuccessResult();
+//    }
+//
+//    @PostMapping("/update")
+//    public Result update(Article article) {
+//        articleService.update(article);
+//        return ResultGenerator.genSuccessResult();
+//    }
 
     @PostMapping("/detail")
     public Result detail(@RequestParam Integer id) {
