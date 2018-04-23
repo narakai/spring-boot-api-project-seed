c0173abaa87cee293db379da05333637539f0fad
laileon <leon@igola.com>
2018-04-23 17:04:10 +0800

    schedule issue

--- src/main/java/com/company/project/schedule/ScheduledTasks.java
+++ src/main/java/com/company/project/schedule/ScheduledTasks.java
@@ -3,6 +3,9 @@ package com.company.project.schedule;
 import com.company.project.model.Article;
 import com.company.project.service.ArticleService;
 import com.google.gson.Gson;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -14,42 +17,52 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.*;
 
 @Component
+@EnableAsync
 public class ScheduledTasks {
     private static final String NHK = "KHNPJ";
     @Resource
     private ArticleService articleService;
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     //test 1 min
     @Scheduled(cron = "0 35 10 * * 1-5")
-    public void timerToNow1() {
-
-        System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        String url = "http://www3.nhk.or.jp/news/easy/news-list.json";
-        String result = restTemplate.getForObject(url, String.class, new HashMap<>());
-        getArticleData(result);
-//        runShell("k10011351191000");
-//        runShell("k10011382561000");
-//        System.out.println(result);
+    public void timerToNow1() throws InterruptedException, ExecutionException, TimeoutException {
+        Future<Object> result = doSomething();
+        result.get(3000000, TimeUnit.MILLISECONDS);
     }
 
     //test 1 min
     @Scheduled(cron = "0 35 14 * * 1-5")
-    public void timerToNow2() {
+    public void timerToNow2() throws InterruptedException, ExecutionException, TimeoutException {
+        Future<Object> result = doSomething();
+        result.get(3000000, TimeUnit.MILLISECONDS);
+    }
+
+    @Async
+    Future<Object> doSomething() {
+        return (Future<Object>) executor.submit(() -> {
+            runTask();
+        });
+    }
+
+    private int runTask() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(10000);
+        requestFactory.setReadTimeout(10000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
         System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         String url = "http://www3.nhk.or.jp/news/easy/news-list.json";
         String result = restTemplate.getForObject(url, String.class, new HashMap<>());
-        getArticleData(result);
-//        runShell("k10011351191000");
-//        runShell("k10011382561000");
-//        System.out.println(result);
+        return getArticleData(result);
     }
 
-    private void getArticleData(String article) {
+    private int getArticleData(String article) {
+        int returnCode = -1;
         List<Article> articleListDB = articleService.findAll();
         System.out.println("DB " + articleListDB.size());
         List<Article> articleList = new ArrayList<>();
@@ -76,16 +89,17 @@ public class ScheduledTasks {
             if (articleList.size() > 0) {
                 for (Article article1 : articleList) {
                     System.out.println(" article1 " + article1.getNews_id());
-                    runShell(article1.getNews_id());
+                    returnCode += runShell(article1.getNews_id());
                 }
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        return returnCode;
     }
 
-    private synchronized void runShell(String newsId) {
+    private synchronized int runShell(String newsId) {
         int returnCode = -1;
         try {
 //            /Users/laileon/Documents/test.sh
@@ -120,5 +134,6 @@ public class ScheduledTasks {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        return returnCode;
     }
 }
