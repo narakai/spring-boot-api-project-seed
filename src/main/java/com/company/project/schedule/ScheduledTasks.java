664334eadd0982dc98f964943454855c1dcc08ab
laileon <leon@igola.com>
2019-08-31 01:21:58 +0800

    google cloud

--- src/main/java/com/company/project/schedule/ScheduledTasks.java
+++ src/main/java/com/company/project/schedule/ScheduledTasks.java
@@ -2,21 +2,32 @@ package com.company.project.schedule;
 
 import com.company.project.model.Article;
 import com.company.project.service.ArticleService;
+import com.google.api.client.util.DateTime;
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.speech.v1.*;
+import com.google.cloud.storage.*;
 import com.google.gson.Gson;
+import com.google.protobuf.ByteString;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.concurrent.*;
 
 @Component
@@ -27,6 +38,90 @@ public class ScheduledTasks {
     private ArticleService articleService;
 
     private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+//    @Scheduled(cron = "0/10 * * * * 1-7")
+    public void getAudioFile() throws Exception {
+//        uploadFile2("/Users/laileon/Documents/ftp/test2.flac", "test2.flac");
+        asyncRecognizeGcs("gs://nhknews-8bad0.appspot.com/test.flac");
+    }
+
+    public static String uploadFile2(String uri, String fileName) throws IOException {
+        Path path = Paths.get(uri);
+        String contentType = "audio/x-flac";
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (final IOException e) {
+        }
+        MultipartFile result = new MockMultipartFile(fileName,
+                fileName, contentType, content);
+
+
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        //文件名
+//        final String fileName = result.getOriginalFilename().substring(result.getOriginalFilename().indexOf("."));
+//        System.err.println("fileName:" + fileName);
+
+        //上传文件
+        BlobInfo blobInfo =
+                storage.create(
+                        BlobInfo
+                                .newBuilder("nhknews-8bad0.appspot.com", fileName)
+                                // Modify access list to allow all users with link to read file
+                                .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                                .setContentType("audio/x-flac")
+                                .build(),
+                        result.getInputStream());
+
+        //System.out.println("下载连接："+blobInfo.getMediaLink());
+        String fileUrl = "https://storage.googleapis.com/" + "nhknews-8bad0.appspot.com" + "/" + fileName;
+        System.out.println(fileUrl);
+        return fileUrl;
+    }
+
+
+    /**
+     * Performs non-blocking speech recognition on remote FLAC file and prints the transcription.
+     *
+     * @param gcsUri the path to the remote LINEAR16 audio file to transcribe.
+     */
+    public static void asyncRecognizeGcs(String gcsUri) throws Exception {
+        // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
+        try (SpeechClient speech = SpeechClient.create()) {
+
+            // Configure remote file request for Linear16
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(RecognitionConfig.AudioEncoding.FLAC)
+                            .setLanguageCode("ja-JP")
+                            .setEnableAutomaticPunctuation(true)
+                            .setSampleRateHertz(16000)
+                            .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(gcsUri).build();
+
+            // Use non-blocking call for getting file transcription
+            OperationFuture<LongRunningRecognizeResponse, LongRunningRecognizeMetadata> response =
+                    speech.longRunningRecognizeAsync(config, audio);
+            while (!response.isDone()) {
+                System.out.println("Waiting for response...");
+                Thread.sleep(10000);
+            }
+
+            List<SpeechRecognitionResult> results = response.get().getResultsList();
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Users/laileon/Documents/trans.txt"))) {
+                for (SpeechRecognitionResult result : results) {
+                    // There can be several alternative transcripts for a given chunk of speech. Just use the
+                    // first (most likely) one here.
+                    SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                    System.out.printf("Transcription: %s\n", alternative.getTranscript());
+                    writer.write(alternative.getTranscript());
+                    writer.write("\n");
+                }
+            } catch (IOException e) {
+            }
+        }
+}
 
     //test 1 min
     @Scheduled(cron = "0 0/10 10-18 * * 1-5")
